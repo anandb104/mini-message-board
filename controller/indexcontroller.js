10ab1685f9b9ba5a26e42e129172bcbb6d10ee29
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-04-01 22:36:26 +0530

    change the basic errors in queries of sql The basic errors in the queries of sql was rectified and the env file was included in the gitignore

--- controller/indexcontroller.js
+++ controller/indexcontroller.js
@@ -1,9 +1,9 @@
 const indexrouter=require("../routes/indexrouter");
 const pool=require("../db/pool.js");
 exports.indexlistget=async(req,res)=>{
-    const rows=await pool.query('SELECT * FROM messages');
+    const result=await pool.query('SELECT * FROM messages');
     res.render("index",{
-        messages:rows
+        messages:result.rows
     });
 };
 exports.newformget=(req,res)=>{
@@ -12,12 +12,12 @@ exports.newformget=(req,res)=>{
 exports.newformpost=async(req,res)=>{
     const name=req.body.name;
     const message=req.body.message;
-    await pool.query('INSERT INTO messages (text,user_name) VALUE ($1,$2)',[message,name]);
+    await pool.query('INSERT INTO messages (text,user_name) VALUES ($1,$2)',[message,name]);
     res.redirect("/");
 };
 exports.messageget=async(req,res)=>{
     const id=req.params.id;
-    const row=pool.query('SELECT * FROM messages WHERE id=$1',[id-1]);
+    const row=await pool.query('SELECT * FROM messages WHERE id=$1',[id-1]);
     const message=row[0];
     res.render("message",{message});
 };
