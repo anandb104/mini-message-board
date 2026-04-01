808de2e78213f678bf5dd002a77638a17b6ca9d2
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-04-01 23:06:08 +0530

    Change the port of the app

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express=require('express');
 const app=express();
 const path = require("node:path");
@@ -6,11 +7,9 @@ app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 const indexrouter=require("./routes/indexrouter");
 app.use("/",indexrouter);
-app.listen(3000,(error)=>{
-    if(error){
-        throw error;
-    }
-    console.log("Listening on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Listening on port ${PORT}`);
 });
 
 
