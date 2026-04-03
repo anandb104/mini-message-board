634b5c0a1a93a1be106d1e956ba24c62f054b6e3
anandb104 <210169445+anandb104@users.noreply.github.com>
2026-04-03 13:47:23 +0530

    Change the layout of the index page
    The layout of the index page was changed with css with the help of grid and flexbox

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app=express();
 const path = require("node:path");
 app.use(express.urlencoded({extended:true}));
 app.set("views",path.join(__dirname,"views"));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static("images"));
 app.set("view engine","ejs");
 const indexrouter=require("./routes/indexrouter");
 app.use("/",indexrouter);
